8f33dfa6378df6478bc8e3f7b990051af409dc1d
Raneesh <raneeshgopakumar@gmail.com>
2026-03-17 03:34:59 +0000

    keypair created

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+
+  description = "my aws region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+
+  description = "my project name"
+  type        = string
+  default     = "zomato"
+}
+
+variable "project_environment" {
+
+  description = "my project environment"
+  type        = string
+  default     = "production"
+}
